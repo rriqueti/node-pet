95e051cb175d6aa4c350456eca13418323c4e561
rriqueti <r.riqueti@gmail.com>
2024-06-12 16:01:05 -0300

    update main

--- main.js
+++ main.js
@@ -3,22 +3,11 @@ const app = express();
 const routes = require('./routes/main');
 const port = 3000;
 
-
-
 //config engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-// const register_interested = require('./routes/createInterested');
-// const checkEmail = require('./routes/checkEmail');
-
-
 app.use(routes);
-
-// app.use('/', checkEmail);
-// app.use('/register', register_interested);
-
-
 
 app.listen(port, ()=> {
     console.log(`Listening on port ${port}`)
