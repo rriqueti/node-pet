e6025605f73b156b9b05a5069caa3f2d3cbb7329
rriqueti <r.riqueti@gmail.com>
2024-06-13 15:12:20 -0300

    change configures main app

--- routes/main.js
+++ routes/main.js
@@ -1,12 +1,20 @@
-const router = require('express').Router();
-const register_interested = require('./createInterested');
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+const register_create = require('./create');
 const checkEmail = require('./checkEmail');
+const checkAuth = require('../utils/checkAuth')
 
+//Allow users access folder Public
+router.use(express.static(path.join(process.cwd(), 'Public')));
 
-// const apiRoutes = require('./api');
+////Allow users access folder Private
+//Check also if user is authenticated 
+// router.use(checkAuth, express.static(path.join(process.cwd(), 'Private')));
+router.use(express.static(path.join(process.cwd(), 'Private')));
 
 router.use('/', checkEmail);
-router.use('/register', register_interested);
+router.use('/register', register_create);
 
 module.exports = router;
 
