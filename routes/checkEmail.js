5773aec15ee374f55188636a2693eab57168fdc0
rriqueti <r.riqueti@gmail.com>
2024-06-13 15:10:37 -0300

    add cokkies config

--- routes/checkEmail.js
+++ routes/checkEmail.js
@@ -1,26 +1,39 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const session = require('express-session');
 const router = express.Router();
 
 router.use(bodyParser.urlencoded({ extended: false }));
 
 router.get('/', (req, res)=>{
-    res.render('./login/login.ejs');
+    res.render('../views/Public/login.ejs');
 });
 
 router.post('/', function (req, res, next) {
     let login = req.body.login;
     let password = req.body.password;
 
-    if(login == 'admin'){
+    //Validate login and password
+    if(login == 'admin' && password == '123'){
+        req.session.autenticatedUser = true;
+        res.cookie('lastRequest', new Date().toLocaleDateString(),{
+            httpOnly: true,
+            maxAge: 1000 * 60 * 60 * 24 * 30,
+        });
+
         console.log('Authorized');
+
+        res.redirect('../views/Private/petList.ejs')
         next();
 
     }
-
     else{
         res.status(500).send('Not Authorized')
     }
+
+
 });
+
+
 
 module.exports = router
